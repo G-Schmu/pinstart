1b3d8813499c1ab7db2049fd783c743fe283f808
Graham.Schmuland <gr382705@dal.ca>
2021-02-13 15:40:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,15 @@
 	<title>pinstart - Find your next business.</title>
 </head>
 <body>
+	<div>
+		
+	</div>
+	<div>
+		<form name="search">
+		<h1>Where's the best location for a new</h1>
+			<input type="search" name="businesssearch" placeholder="Business Name/Idea">
+			<h2>in</h2><input type="text" name="city" value="Calgary" readonly>
+		</form>
+	</div>
 </body>
 </html>
