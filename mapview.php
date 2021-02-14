f0da1493296c59a8759c60180d3d449a5025d70b
Graham.Schmuland <gr382705@dal.ca>
2021-02-13 18:16:34 -0700

    Map Test 1

--- mapview.php
+++ mapview.php
@@ -7,6 +7,33 @@
   <head>
   <title>pinstart - Find your next business.</title>
   <link rel="stylesheet" type="text/css" href="main.css">
+  <script type="text/javascript">
+    // Initialize and add the map
+    function initMap(): void {
+      // The location of Uluru
+      const uluru = { lat: -25.344, lng: 131.036 };
+      // The map, centered at Uluru
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          zoom: 4,
+          center: uluru,
+        }
+      );
+
+      // The marker, positioned at Uluru
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+      });
+    }
+  </script>
+  <script
+        src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
+        defer
+      ></script>
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+      <script src="./index.js"></script>
   </head>
   <body class="mapviewph">
     <div>
@@ -27,5 +54,7 @@
       <h4>Here's why:</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lectus metus, facilisis vitae tellus nec, ullamcorper auctor ipsum. Curabitur cursus laoreet euismod. Cras vehicula, neque eget consequat cursus, quam dui tempor turpis, at sollicitudin orci purus egestas nulla. Sed sed sagittis neque. Curabitur a ipsum congue, rutrum est eu, eleifend diam. Quisque maximus at tellus sit amet bibendum. Donec tincidunt sit amet leo in ultrices. Morbi eget diam tincidunt, ultricies nibh eu, varius nisi. Sed a porttitor ligula. Nulla ut lacus feugiat ipsum pharetra laoreet. Etiam sed tellus eu tortor facilisis interdum.</p>
     </div>
+
+    <div id="map"></div>
   </body>
 </html>
