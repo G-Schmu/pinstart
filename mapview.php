716cb9d3ee2ef5c1598fa454a00ecf8a3a3d50d6
Graham.Schmuland <gr382705@dal.ca>
2021-02-13 18:20:48 -0700

    Update mapview.php

--- mapview.php
+++ mapview.php
@@ -8,28 +8,17 @@
   <title>pinstart - Find your next business.</title>
   <link rel="stylesheet" type="text/css" href="main.css">
   <script type="text/javascript">
-    // Initialize and add the map
-    function initMap(): void {
-      // The location of Uluru
-      const uluru = { lat: -25.344, lng: 131.036 };
-      // The map, centered at Uluru
-      const map = new google.maps.Map(
-        document.getElementById("map") as HTMLElement,
-        {
-          zoom: 4,
-          center: uluru,
-        }
-      );
+    let map;
 
-      // The marker, positioned at Uluru
-      const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
+    function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
       });
     }
   </script>
   <script
-        src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd3MjsurBTEZFEF7aZkPWGBPVXRZvU_QM&callback=initMap&libraries=&v=weekly"
         defer
       ></script>
       <link rel="stylesheet" type="text/css" href="./style.css" />
