69456b5dfe9efc8fa33cc1c353b7d272d9293155
Graham.Schmuland <gr382705@dal.ca>
2021-02-13 19:17:54 -0700

    smh

--- mapview.php
+++ mapview.php
@@ -22,28 +22,31 @@
   <script src="./index.js"></script>
   </head>
   <body>
-    <nav id="mainbar">
-      <a href="index.php"><img src="pinstart-icon.png" ></a>
-      <input type="search" name="businesssearch">
-      <h2>in</h2>
-      <input type="text" value="Calgary" readonly>
-      <img src="right-arrow.png" alt="->">
-    </nav>
+    <div class="wrapper">
 
-    <div id="recContainer">
-      <h2>Recommended</h2>
-      <hr>
-      <h3>1108 Memorial Dr NW</h3>
-      <hr>
-      <h4>Here's why:</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lectus metus, facilisis vitae tellus nec, ullamcorper auctor ipsum. Curabitur cursus laoreet euismod. Cras vehicula, neque eget consequat cursus, quam dui tempor turpis, at sollicitudin orci purus egestas nulla. Sed sed sagittis neque. Curabitur a ipsum congue, rutrum est eu, eleifend diam. Quisque maximus at tellus sit amet bibendum. Donec tincidunt sit amet leo in ultrices. Morbi eget diam tincidunt, ultricies nibh eu, varius nisi. Sed a porttitor ligula. Nulla ut lacus feugiat ipsum pharetra laoreet. Etiam sed tellus eu tortor facilisis interdum.</p>
-    </div>
-    <div>
       <div id="map"></div>
       <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd3MjsurBTEZFEF7aZkPWGBPVXRZvU_QM&callback=initMap&libraries=&v=weekly"
         async
       ></script>
+      
+      <nav id="mainbar">
+        <a href="index.php"><img src="pinstart-icon.png" ></a>
+        <input type="search" name="businesssearch">
+        <h2>in</h2>
+        <input type="text" value="Calgary" readonly>
+        <img src="right-arrow.png" alt="->">
+      </nav>
+
+      <div id="recContainer">
+        <h2>Recommended</h2>
+        <hr>
+        <h3>1108 Memorial Dr NW</h3>
+        <hr>
+        <h4>Here's why:</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lectus metus, facilisis vitae tellus nec, ullamcorper auctor ipsum. Curabitur cursus laoreet euismod. Cras vehicula, neque eget consequat cursus, quam dui tempor turpis, at sollicitudin orci purus egestas nulla. Sed sed sagittis neque. Curabitur a ipsum congue, rutrum est eu, eleifend diam. Quisque maximus at tellus sit amet bibendum. Donec tincidunt sit amet leo in ultrices. Morbi eget diam tincidunt, ultricies nibh eu, varius nisi. Sed a porttitor ligula. Nulla ut lacus feugiat ipsum pharetra laoreet. Etiam sed tellus eu tortor facilisis interdum.</p>
+      </div>
+      
     </div>
   </body>
 </html>
