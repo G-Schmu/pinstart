8e85afaa2cd9a5d8205498bdeb6e975261f4592e
Graham.Schmuland <gr382705@dal.ca>
2021-02-13 17:07:29 -0700

    Initial Layouts and Images

--- index.php
+++ index.php
@@ -6,17 +6,19 @@
 <html>
 <head>
 	<title>pinstart - Find your next business.</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-	<div>
-		
-	</div>
-	<div>
-		<form name="search">
-		<h1>Where's the best location for a new</h1>
-			<input type="search" name="businesssearch" placeholder="Business Name/Idea">
-			<h2>in</h2><input type="text" name="city" value="Calgary" readonly>
-		</form>
-	</div>
+	<div class="aligncenter" id="indexmenu">
+		<img src="pinstart-2.png" width="400em" id="indexmainlogo">
+		<div id="searchbar">
+			<form name="search">
+			<h1>Where's the best location for a new</h1>
+				<div class="hor"><input type="search" id="indexsearch" name="businesssearch" placeholder="Business Name/Idea">
+        		<button id="right-arrow"></div>
+				<div class="hor"><h2>in</h2><input type="text" value="Calgary" readonly></div>
+			</form>
+		</div>
+	</div>	
 </body>
 </html>
--- mapview.php
+++ mapview.php
@@ -5,17 +5,27 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Add Map</title>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd3MjsurBTEZFEF7aZkPWGBPVXRZvU_QM&callback=initMap&libraries=&v=weekly"
-      defer
-    ></script>
-    <link rel="stylesheet" type="text/css" href="./style.css" />
-    <script src="./index.js"></script>
+  <title>pinstart - Find your next business.</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
   </head>
-  <body>
-    <h3>My Google Maps Demo</h3>
-    <!--The div element for the map -->
-    <div id="map"></div>
+  <body class="mapviewph">
+    <div>
+      <nav id="mainbar">
+        <img src="pinstart-icon.png" >
+        <input type="search" name="businesssearch">
+        <h2>in</h2>
+        <input type="text" value="Calgary" readonly>
+        <img src="right-arrow.png" alt="->">
+      </nav>
+    </div>
+
+    <div id="recContainer">
+      <h2>Recommended</h2>
+      <hr>
+      <h3>1108 Memorial Dr NW</h3>
+      <hr>
+      <h4>Here's why:</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lectus metus, facilisis vitae tellus nec, ullamcorper auctor ipsum. Curabitur cursus laoreet euismod. Cras vehicula, neque eget consequat cursus, quam dui tempor turpis, at sollicitudin orci purus egestas nulla. Sed sed sagittis neque. Curabitur a ipsum congue, rutrum est eu, eleifend diam. Quisque maximus at tellus sit amet bibendum. Donec tincidunt sit amet leo in ultrices. Morbi eget diam tincidunt, ultricies nibh eu, varius nisi. Sed a porttitor ligula. Nulla ut lacus feugiat ipsum pharetra laoreet. Etiam sed tellus eu tortor facilisis interdum.</p>
+    </div>
   </body>
 </html>
